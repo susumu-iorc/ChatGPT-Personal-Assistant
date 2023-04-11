1ca9c8cfca466693dce077d07891546805b38316
susumu <susumu@iorc.net>
2023-04-12 00:30:41 +0900

    ベースを作った

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { Configuration, OpenAIApi } from "openai";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export async function ask(content: string, model = "gpt-3.5-turbo-0301") {
+  const response = await openai.createChatCompletion({
+    model: model,
+    messages: [{ role: "user", content: content }],
+  });
+
+  const answer = response.data.choices[0].message?.content;
+  console.log(answer);
+}
+
+const question = 'cout << "Hello World" << endl';
+ask(question);
